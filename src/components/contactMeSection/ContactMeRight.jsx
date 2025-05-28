9cb73cc785cc751fb5793c79eb715b5ba6db80cd
OBANIJESU <obanijesu@OBANIJESUs-MacBook-Pro.local>
2025-05-28 17:11:40 +0100

    Refactor image imports and update personal information across multiple components

--- src/components/contactMeSection/ContactMeRight.jsx
+++ src/components/contactMeSection/ContactMeRight.jsx
@@ -1,11 +1,12 @@
 import ContactInfo from "./ContactInfo";
 import ContactSocial from "./ContactSocial";
+import EmailMe from "../../assets/images/email-me.png";
 
 const ContactMeRight = () => {
   return (
     <div className="flex flex-col items-center justify-center gap-12">
       <img
-        src="../../public/images/email-image.png"
+        src={EmailMe}
         alt="email image"
         className="max-w-[300px]"
       />
